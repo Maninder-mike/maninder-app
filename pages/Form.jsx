123b7210c5e75f384fcd0150dba5a2fb9a8d99ff
maninder-mike <maninder.singh@outlook.in>
2022-06-23 18:22:02 -0400

    second commit

--- pages/Form.jsx
+++ pages/Form.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <div>
+      <input type="text" placeholder="Name" />
+      <input type="email" placeholder="Email" />
+      <button>Click Here</button>
+    </div>
+  );
+};
 
-export default Form
+export default Form;
