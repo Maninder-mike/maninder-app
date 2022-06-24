4420704f51a0f2425cf9a90c79a5f122bf5bfb11
maninder-mike <maninder.singh@outlook.in>
2022-06-23 22:46:58 -0400

    test commit

---
+++ pages/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.nav}>
+      <div>Home</div>
+    </nav>
+  );
+};
+
+export default Navbar;
