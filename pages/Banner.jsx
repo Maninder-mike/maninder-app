48e565e93128369af3aece0620dbf9116d99eb02
maninder-mike <maninder.singh@outlook.in>
2022-08-21 16:53:28 -0400

    web site working soon

--- pages/Banner.jsx
+++ pages/Banner.jsx
@@ -4,8 +4,8 @@ const Banner = () => {
   return (
     <>
       <div className="bg-blue-500 text-center font-bold place-content-center text-8xl font-mono">
-        <div className="pt-50  tracking-wider place-content-center">
-          Hello World!
+        <div className="pt-50 tracking-wider place-content-center">
+          Website Under Construction
         </div>
       </div>
     </>
